9c295f734938a54d6b8438d5e21256d512246938
bruno <delgado.hebru24@triana.salesianos.edu>
2023-11-26 12:15:19 +0100

    Depuración

---
+++ PruebaEEDD/src/prueba/PruebaDebugger.java
@@ -0,0 +1,14 @@
+package prueba;
+
+public class PruebaDebugger {
+	 
+	public static void main(String[] args) {
+    	ClaseParaDepurar clase = new ClaseParaDepurar();
+    	double valor = clase.echaCuentas();
+    	System.out.println("Sale "+valor);
+    }
+ 
+}
+
+
+
